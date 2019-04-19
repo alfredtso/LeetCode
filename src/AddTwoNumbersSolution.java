6a85499e7173659dff5ba26dda91be95d3d5484f
Alfred Tso <alfredtso@gmail.com>
2019-04-20 02:45:18 +0800

    Added LengthOfLongestSubstring

--- src/AddTwoNumbersSolution.java
+++ src/AddTwoNumbersSolution.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+class addTwoNumbers {
 
-class ListNode {
+    class ListNode {
         int val;
         ListNode next;
 
@@ -15,94 +16,35 @@ class ListNode {
         }
     }
 
-    class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode p = l1, q = l2, current = result;
+        int carry = 0;
 
-            ListNode result = new ListNode(0);
-            ListNode p = l1, q = l2, current = result;
-            int carry = 0;
+        while (p != null || q != null) {
+            // calculate the digit
+            int x = p != null ? p.val : 0;
+            int y = q != null ? q.val : 0;
+            int sum = x + y + carry;
 
-            while (p != null || q != null) {
-                // calculate the digit
-                int x = p != null ? p.val : 0;
-                int y = q != null ? q.val : 0;
-                int sum = x + y + carry;
+            // update carry
+            carry = sum / 10;
 
-                // update carry
-                carry = sum / 10;
+            current.next = new ListNode(sum % 10);
 
-                current.next = new ListNode(sum % 10);
+            // update pointer
+            current = current.next;
 
-                // update pointer
-                current = current.next;
-
-                if (p != null) {p = p.next;}
-                if (q != null) {q = q.next;}
-            }
-
-            if (carry > 0) {current.next = new ListNode(carry);}
-
-            return result.next;
+            if (p != null) {p = p.next;}
+            if (q != null) {q = q.next;}
         }
 
-        public static int[] stringToIntegerArray(String input) {
-            input = input.trim();
-            input = input.substring(1, input.length() - 1);
-            if (input.length() == 0) {
-                return new int[0];
-            }
+        if (carry > 0) {current.next = new ListNode(carry);}
 
-            String[] parts = input.split(",");
-            int[] output = new int[parts.length];
-            for (int index = 0; index < parts.length; index++) {
-                String part = parts[index].trim();
-                output[index] = Integer.parseInt(part);
-            }
-            return output;
-        }
-
-        public static ListNode stringToListNode(String input) {
-            // Generate array from the input
-            int[] nodeValues = stringToIntegerArray(input);
-
-            // Now convert that list into linked list
-            ListNode dummyRoot = new ListNode(0);
-            ListNode ptr = dummyRoot;
-            for (int item : nodeValues) {
-                ptr.next = new ListNode(item);
-                ptr = ptr.next;
-            }
-            return dummyRoot.next;
-        }
-
-        public static String listNodeToString(ListNode node) {
-            if (node == null) {
-                return "[]";
-            }
-
-            String result = "";
-            while (node != null) {
-                result += Integer.toString(node.val) + ", ";
-                node = node.next;
-            }
-            return "[" + result.substring(0, result.length() - 2) + "]";
-        }
-
-        public static void main(String[] args) throws IOException {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            String line;
-            while ((line = in.readLine()) != null) {
-                ListNode l1 = stringToListNode(line);
-                line = in.readLine();
-                ListNode l2 = stringToListNode(line);
-
-                ListNode ret = new Solution().addTwoNumbers(l1, l2);
-
-                String out = listNodeToString(ret);
-
-                System.out.print(out);
-            }
-        }
+        return result.next;
     }
+}
+
+
 
--- src/Ex3.java
+++ src/Ex3.java
@@ -1,14 +1,101 @@
-import org.junit.Test;
+import sun.text.normalizer.Trie;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
 
-    public int lengthOfLongestSubstring(String s) {
+    // Brute Force
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (allUnique(s, i, j)) {
+                    ans = Math.max(ans, j - i);
+                }
+            }
+        }
+        return ans;
     }
 
+    private static boolean allUnique(String s, int i, int j) {
+        Set<Character> characterSet = new HashSet<>();
+        for (int k = i; k < j; k++) {
+            char ch = s.charAt(k);
+            if (characterSet.contains(ch)) return false;
+            characterSet.add(ch);
+        }
+        return true;
+    }
 
+    public static int lengthSlidingWindow(String s) {
+        int n = s.length();
+        int ans = 0;
+        Set<Character> set = new HashSet<>();
+        int i = 0, j = 0;
+        while (i < n && j < n) {
+            char ch = s.charAt(j);
+            if (!set.contains(ch)) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, set.size());
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    public static int lengthHashMap(String s) {
+        int n = s.length();
+        int ans = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0, j = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j+1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String ex1 = "abcabcab";
+        String ex2 = "bbbbb";
+        String ex3 = "pwwkew";
+
+        if (lengthOfLongestSubstring(ex1) != 3) {
+            System.out.println("Example 1 failed");
+        }
+        if (lengthOfLongestSubstring(ex2) != 1) {
+            System.out.println("Example 2 failed");
+        }
+        if (lengthOfLongestSubstring(ex3) != 3) {
+            System.out.println("Example 3 failed");
+        }
+
+        if (lengthSlidingWindow(ex1) != 3) {
+            System.out.println("Example 1 failed");
+        }
+        if (lengthSlidingWindow(ex2) != 1) {
+            System.out.println("Example 2 failed");
+        }
+        if (lengthSlidingWindow(ex3) != 3) {
+            System.out.println("Example 3 failed");
+        }
+
+        if (lengthHashMap(ex1) != 3) {
+            System.out.println("Example 1 failed");
+        }
+        if (lengthHashMap(ex2) != 1) {
+            System.out.println("Example 2 failed");
+        }
+        if (lengthHashMap(ex3) != 3) {
+            System.out.println("Example 3 failed");
+        }
+    }
 }
 
-class SolutionTest {
-
-}
